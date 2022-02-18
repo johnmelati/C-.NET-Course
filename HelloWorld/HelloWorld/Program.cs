c5503ca577a2f750933afa75602019440bd6b6a1
johnmelati <john.melati@gmail.com>
2022-02-18 01:02:30 -0300

    primeiro programa

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Para comentar utiliza-se "//" ou ctrl+k+c (comenta) ctrl+k+u (descomenta)
+            Console.Write("Hello World!\n");
+            Console.Write("Meu nome é John\n");
+            Console.WriteLine("Hello World!\n"); //escreve o comando e quebra a linha
+            Console.WriteLine("Meu nome é John\n");
+            Console.ReadLine();
+        }
+    }
+}
